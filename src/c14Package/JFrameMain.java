a871f3aaad1ac5e22ace0463ca316e14b6d5eb6f
Braines <Braines@WS10TC11104.dbq.int>
2018-05-13 13:55:54 -0500

    Inital Push

---
+++ src/c14Package/JFrameMain.java
@@ -0,0 +1,317 @@
+/**
+ *   file: JFrameMain.java
+ */
+package c14Package;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.Label;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+/**
+ * @author Brandon Raines
+ * 
+ *  Chapter 14 Excercises 
+ *
+ */
+public class JFrameMain extends JFrame {
+
+	private JPanel contentPane;
+	
+	// Global variables
+	int[] myList;  // this will contain integers to be sorted
+	
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JFrameMain frame = new JFrameMain();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public JFrameMain() {
+		setTitle("Brandon Raines Chapter 14 Exercises");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1131, 728);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(632, 75, 439, 565);
+		contentPane.add(scrollPane);
+		
+		JTextArea txtAreaSortoutput = new JTextArea();
+		scrollPane.setViewportView(txtAreaSortoutput);
+		txtAreaSortoutput.setFont(new Font("Monospaced", Font.PLAIN, 24));
+		txtAreaSortoutput.setText("SortOutput here...");
+
+		
+		
+		JButton btnSorting1 = new JButton("Sorting 1");
+		btnSorting1.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		btnSorting1.setBounds(12, 75, 220, 53);
+		contentPane.add(btnSorting1);
+		btnSorting1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				// Simple sorting practice
+				// create an array to be processed
+				// integers first - call it list
+				
+				// statically initialize the list
+				int[] list = { 16, 30, 24, 7, 62, 45, 5, 55 };
+				
+				// list all elements before sort
+				txtAreaSortoutput.append("list[] before sort ...\n ");
+				for (int i=0; i<list.length; i++)
+					txtAreaSortoutput.append(list[i] + ", ");
+				// send this list to our sorting class
+				Sorting.selectionSort(list);
+				
+				// display the results somewhere
+				txtAreaSortoutput.append("\n" + list.toString() + "\n"); // the lazy way to display an array
+				for( int i=0; i<list.length; i++)
+					txtAreaSortoutput.append(list[i] + ", ");
+				txtAreaSortoutput.append("\n \n");
+			}
+		});
+		
+
+		Label label = new Label("This program demonstrates sorting of arrays and lists.");
+		label.setFont(new Font("Dialog", Font.PLAIN, 18));
+		label.setBounds(10, 10, 1061, 59);
+		contentPane.add(label);
+		
+		JButton btnExerciseBinary = new JButton("Exercise 14-1 Binary Search test");
+		btnExerciseBinary.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				// sort myList
+				// display the first 50 items in the list
+				// search for an item in myList
+				// display the result of the search
+				
+			}
+		});
+		btnExerciseBinary.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		btnExerciseBinary.setBounds(12, 257, 551, 53);
+		contentPane.add(btnExerciseBinary);
+		
+		JButton btnCreateAList = new JButton("Create an int[] list");
+		btnCreateAList.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				//creates random numbers in the Array myList
+				myList = new int [1000];
+				for (int i = 0; i < myList.length; i++)
+						myList[i] = (int) (Math.random() * 1000+1);
+						
+						// Display the first 50 values of my list in the output area
+						String str = "myList[] - First 50 Random Numbers: \n";
+							for (int i = 0; i < 50; i++)
+								str += myList[i] + ", ";
+							txtAreaSortoutput.append("\n" + str);
+						
+				
+				// build an array of random integers between 1 and 1000
+				
+				// Write the code to generate a list of random numbers
+				// allocate 1000 items in myList
+				// put the values in myList using the Math.random() method
+				// display the first 50 values of myList in the output JTextArea
+				
+				
+			}
+		});
+		btnCreateAList.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		btnCreateAList.setBounds(12, 141, 248, 53);
+		contentPane.add(btnCreateAList);
+		
+		JButton btnExerciseRemove = new JButton("Exercise 14-2 remove() test");
+		btnExerciseRemove.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				// create a test array 
+				int[] testArray = {1,2,3,4,5,6,7};
+				txtAreaSortoutput.append("\n Test Array: \n");
+				for (int i = 0; i < testArray.length; i++)
+						txtAreaSortoutput.append(testArray[i] + ",");
+				
+				// remove an item
+				
+				int newLength = remove(testArray, testArray.length, 4);
+				
+				if (newLength == testArray.length)
+						txtAreaSortoutput.append("\n item 4 not found! \n");
+				
+				else {
+					txtAreaSortoutput.append("\n after removing 4: \n");
+					for (int i = 0; i < newLength; i++)
+						txtAreaSortoutput.append(testArray[i] + ", ");
+					
+				}
+		
+				
+			}
+		});
+		btnExerciseRemove.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		btnExerciseRemove.setBounds(12, 323, 551, 53);
+		contentPane.add(btnExerciseRemove);
+		
+		JButton btnExerciseRemoveat = new JButton("Exercise 14-3 removeAt() test");
+		btnExerciseRemoveat.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				// create a test array 
+				int[] testArray = {1,2,3,4,5,6,7};
+				txtAreaSortoutput.append("\n Test Array: \n");
+				for (int i = 0; i < testArray.length; i++)
+						txtAreaSortoutput.append(testArray[i] + ",");
+				
+				// remove an item
+				
+				int newLength = removeAt(testArray, testArray.length, 4);
+				
+				if (newLength == testArray.length)
+						txtAreaSortoutput.append("\n item 4 not found! \n");
+				
+				else {
+					txtAreaSortoutput.append("\n after removing 4: \n");
+					for (int i = 0; i < newLength; i++)
+						txtAreaSortoutput.append(testArray[i] + ", ");
+				
+			}
+			}
+		});
+		btnExerciseRemoveat.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		btnExerciseRemoveat.setBounds(12, 389, 551, 53);
+		contentPane.add(btnExerciseRemoveat);
+		
+		JButton btnExerciseRemoveall = new JButton("Exercise 14-4 removeAll() test");
+		btnExerciseRemoveall.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		btnExerciseRemoveall.setBounds(12, 455, 551, 53);
+		contentPane.add(btnExerciseRemoveall);
+		
+		JButton btnExerciseInsertionsort = new JButton("Exercise 14-9 insertionSort() test");
+		btnExerciseInsertionsort.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				long clock_start = System.nanoTime();
+				
+				
+				//Makes sure my list has been filled with random numbers
+				if (null != myList){
+					
+				
+				Sorting.insertionSort(myList);
+				
+				long clock_end = System.nanoTime();
+				
+				// Display the first 50 values of my list in the output area
+				String str = "myList[] - First 50 Random Numbers: \n";
+					for (int i = 0; i < 50; i++)
+						str += myList[i] + ", ";
+					txtAreaSortoutput.append("\n" + str);
+					txtAreaSortoutput.append("\n Time to run: " + (clock_end - clock_start) + "\n");
+					
+				
+		
+					
+					
+					
+				}
+				
+				else{
+					txtAreaSortoutput.append("\n Create Array List First");
+				}
+				
+				
+			}
+		});
+		btnExerciseInsertionsort.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		btnExerciseInsertionsort.setBounds(12, 521, 551, 53);
+		contentPane.add(btnExerciseInsertionsort);
+		
+		JButton btnExerciseInsertionsortvector = new JButton("Exercise 14-10 insertionSortVector() test");
+		btnExerciseInsertionsortvector.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		btnExerciseInsertionsortvector.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		btnExerciseInsertionsortvector.setBounds(12, 587, 551, 53);
+		contentPane.add(btnExerciseInsertionsortvector);
+		
+		JButton btnCreateAVector = new JButton("Create a Vector list");
+		btnCreateAVector.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+				
+			}
+		});
+		btnCreateAVector.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		btnCreateAVector.setBounds(315, 141, 248, 53);
+		contentPane.add(btnCreateAVector);
+	}
+	
+	int remove(int[] a, int len, int rval){
+		boolean found = false;
+		int i = 0;
+		while (! found && i  < len){
+			
+			//comparing I value to increment to I once found true it will stop, if i gets to big/bigger then length it will stop
+				found = rval == a[i++];
+				
+		}
+		int j = i - 1; // set j to location of rval in array a
+		if (found) {
+			
+			// i is pointing to the position of the element 1 to the right of value we want to remove
+			while (i < len)
+				a[j++] = a[i++];
+			return len - 1;
+		
+			
+		}
+		
+		return len;
+		
+	}
+	
+	int removeAt(int[] a, int len, int rLocation){
+	
+			
+			//comparing I value to increment to I once found true it will stop, if i gets to big/bigger then length it will stop
+for ( int i=rLocation; i<len-1;i++){
+	a[i] = a[i+1];
+		
+			
+		}
+		
+		return len-1;
+	
+}
+	
+}
---
+++ src/c14Package/Sorting.java
@@ -0,0 +1,178 @@
+/**
+ *   file: Sorting.java
+ */
+package c14Package;
+
+import java.util.Vector;
+
+/**
+ * Brandon Raines
+ * 
+ * Sorting Class
+ * 
+ *
+ * 
+ * Description: 
+ * 
+ * 	This is the class containing various codes for diffirent excercises from CHP14
+ *
+ */
+public class Sorting {
+
+	
+	// Around page 890
+	public static void selectionSort(int[] list) {
+		// sort the integer array given using a Selection sort
+		
+		
+		// find the index (location) of the smallest item in the list
+		int indexSmallest; // before looping through all elements, first item is smallest
+		
+		
+		
+		for( int Index = 0; Index <list.length -1; Index++ ){
+			// finds index location of smallest item
+			
+				indexSmallest = Index;  // save the location of smaller elementsindexSmallest = Index;
+		
+		// once the for loop is complete, indexSmallest is the location of the first
+		
+				
+				// find smallest item in the rest of list
+		for (int minIndex = Index +1; minIndex < list.length; minIndex++)
+			if(list[minIndex] < list[indexSmallest])
+				indexSmallest = minIndex;
+		//once the inner for the loop is complete, smallest is the location of the first
+		// smallest element in the list (if values can repeat)
+		
+		if (Index != indexSmallest) {
+			// swap the values if index is not smallest
+			
+			int temp = list [Index];
+			list[Index] = list[indexSmallest];
+			list[indexSmallest] = temp;
+			}
+		}
+		
+	}
+	
+	
+	
+	
+		
+		public static void insertionSort( int[] list ) {
+			
+			// This code will sort the int [] list and move items from the left to right
+			// it modififies the list in place
+
+		// see chapter 14 for the necessary code to perform an insertion sort
+
+		int firstOutOfOrder, location;
+
+		int temp;
+
+		//Process every element in the list
+
+		for (firstOutOfOrder = 1; firstOutOfOrder < list.length; firstOutOfOrder++)
+			
+			// if something is out of order it will look for element on the left is larger then element on the right and looks for new spot
+
+			if (list[firstOutOfOrder] < list[firstOutOfOrder - 1]) {
+
+				temp = list[firstOutOfOrder]; // more efficient swapping
+
+				location = firstOutOfOrder;
+
+				do { //shifts elements to the right
+
+					list[location] = list[location - 1];
+
+					location--;
+
+				} while (location > 0 && list[location - 1] > temp);
+
+				list[location] = temp;
+
+			}
+
+			}  // end of insertionSort()
+		
+		// see chapter 14 for the necessary code to perform an insertion sort
+		// include comments
+	
+	
+	
+	// Chapter 14 Programming Exercise 1  part 1
+	// explain the binarySearch algorithm as described on page 918
+	public static int binarySearch(int[] list, int listLength, int searchItem) {
+		
+		// type in the code for the binary search found on page 918
+		// write the code
+		
+		return 0;  // replace this with the correct return statement
+	}
+	
+	
+	// Chapter 14 Programming Exercise 2
+	public static int remove( int[] intArray, int aLength, int removeItem ) {
+		// find and remove the first occurrence of removeItem in intArray
+		
+		int i = 0;
+		boolean found = false;
+		while(!found && i <aLength)
+			found = intArray[i++] == removeItem; // compare increment
+		if(found) {
+			while (i < aLength)
+				intArray[i-1] = intArray[i++]; // assign, increments
+			return aLength - 1; // done
+			
+		} return aLength; 
+		
+		
+		// display a console error where appropriate
+	}
+	
+	
+	// Chapter 14 Programming Exercise 3
+	public static int removeAt( int[] intArray, int aLength, int index )  {
+		// remove item at location index, reducing the length of the array by 1
+		while (index < aLength)
+			intArray[index++] = intArray[index+1];
+		return aLength-1;
+		
+		// display an error message where needed
+	}
+	
+	
+	// Chapter 14 Programming Exercise 4
+	public static void removeAll( int[] intArray, int aLength, int removeItem ) {
+		
+		// delete all occurrences of removeItem from intArray
+	}
+	
+	
+	// Chapter 14 Programming Exercise 9
+	public static void insertionSortVector(Vector<String> vString)  {
+		
+		// add the appropriate Vector type variable to be sorted
+		
+		// Vectors are similar to ArrayLists
+		// iterate through all items in the vector
+		
+		for (int i = 1; i < vString.size(); i++){
+			String temp = vString.get(i);
+			int j = i - 1;
+			
+			// find the correct position for string temp in the vector vString
+			while (0 <= j && vString.get(j).compareTo(temp) > 0){
+				vString.set(j, vString.get(j -1));
+				j --;
+			}
+			
+			vString.set(j + 1, temp);
+			
+		}
+		
+	}
+	
+}
